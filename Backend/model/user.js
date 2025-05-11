98ae4e639982da3c0161d2e4d679c535717def37
ANU-2524 <anusoni25.2006@gmail.com>
2025-05-11 17:43:34 +0530

    Updated

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const userSchema = new./userema({
+const userSchema = new mongoose.Schema({
   name:{
     type: String,
     required: [true, "Please enter your name!"],
